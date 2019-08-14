b8d91047db6f86cfe3a306f2f3614b2842dd41a6
nothrazim <kevin.cederqvist@futuregames.nu>
2019-08-14 16:02:20 +0200

    Initial commit with physics Added left flipper Player input begun.

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    private Camera playerCamera; //Default value null
+
+    private void Awake()
+    {
+        playerCamera = Camera.main;
+    }
+
+    void Start()
+    {
+        Debug.Log("Initializing player input");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
